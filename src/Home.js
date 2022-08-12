fe20b84ccd2da8b0de47193e494333b6ae0df56a
Aaron Hazzard <aaronsploit@gmail.com>
2022-08-11 20:35:34 -0700

    Map through blog-list

--- src/Home.js
+++ src/Home.js
@@ -1,53 +1,27 @@
-//importing useState from react to allow values to be updated
-import { useState} from 'react';
-//function component that stores the home container information.
+import { useState } from 'react';
+
 const Home = () => {
-    const ListofNames = [
-        {
-            name1: 'Aaron',
-            age: 19
-        },
-        {
-            name2: 'Jonathan',
-            age: 20
-        }
+    const [blogs, setBlogs] = useState([
+{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+{ title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
-    ]
-    //reactive variables that changes the state from Aaron to something else
 
-    //Aaron's information
-    const [name_1, setName_1] = useState(ListofNames[0].name1)//name
-    const [age_1, setAge_1] = useState(ListofNames[0].age);//age
 
-    //Jonathan's information
-    const [name_2, setName_2] = useState(ListofNames[1].name2) //name
-    const [age_2, setAge_2] = useState(ListofNames[1].age); //age
-
-    //handleClick function that displays the event that is fired when the user clicks on the button
-    const handleClick = (name, e) => {
-        console.log(`State before: NAME: ${name_1}, AGE ${age_1}`, e.target);
-      
-        //changes the state from Aaron to Jonathan
-        setName_1(ListofNames[1].name2);
-        setAge_1(ListofNames[1].age);
-
-        console.log(`Current State: NAME: ${name_2}, AGE ${age_2}`, e.target);
-    }
-
-    //JSK(Javascript XML) code to render to DOM
-    return ( 
-        /*home container*/ 
+    return (  
         <div className="home">
-           <h1>Homepage</h1>
-           
-<button id="button" onClick={
-    (e) => handleClick('Aaron', e)}>Click Me
-</button>
-<p>Current State of name: { name_1 }</p>
-<p>Current State of age: { age_1 }</p>
+            {blogs.map(blog => (
+<div className="blog-preview" key={blog.id}>
+<h2>{ blog.title }</h2>
+<p>{ blog.author}</p>
+<div className="body">
+    {blog.body}
+</div>
+                </div>
+            ))}
         </div>
-    );
+        );
 }
  
-//exports Home component to be used in other files
 export default Home;
